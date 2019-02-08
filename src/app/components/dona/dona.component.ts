0b17111f3ed413428eec44eab6e2e0dc788559ed
Alan Acuña <alantheace@gmail.com>
2019-02-07 18:21:23 -0600

    Seccion terminada

---
+++ src/app/components/dona/dona.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dona',
+  templateUrl: './dona.component.html',
+  styles: []
+})
+export class DonaComponent {
+  public doughnutChartLabels: string[] = ['Tamales', 'Tortillas', 'Chorizo'];
+  public doughnutChartData: number[] = [350, 450, 100];
+  public doughnutChartType = 'doughnut';
+  // events
+  public chartClicked(e: any): void {
+    console.log(e);
+  }
+  public chartHovered(e: any): void {
+    console.log(e);
+  }
+  random() {
+    this.doughnutChartData = [
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100)
+    ];
+  }
+}
